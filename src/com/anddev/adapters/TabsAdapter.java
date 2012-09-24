94099018a4476a8d5bca917351ea40bdcd5a5c27
mvarnagiris <mantas.varnagiris@gmail.com>
2012-09-24 12:12:16 +0100

    Created TabAdapter for Activities that shows swipeable tabs

---
+++ src/com/anddev/adapters/TabsAdapter.java
@@ -0,0 +1,131 @@
+package com.anddev.adapters;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class TabsAdapter extends FragmentPagerAdapter implements ActionBar.TabListener, ViewPager.OnPageChangeListener
+{
+	private final Context				context;
+	private final ActionBar				actionBar;
+	private final ViewPager				viewPager;
+	private final ArrayList<TabInfo>	tabs	= new ArrayList<TabInfo>();
+
+	public TabsAdapter(SherlockFragmentActivity activity, ViewPager pager)
+	{
+		super(activity.getSupportFragmentManager());
+		this.context = activity;
+		this.actionBar = activity.getSupportActionBar();
+		this.viewPager = pager;
+		this.viewPager.setAdapter(this);
+		this.viewPager.setOnPageChangeListener(this);
+	}
+
+	// Public methods
+	// --------------------------------------------------------------------------------------------------------------------------
+
+	public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args)
+	{
+		TabInfo info = new TabInfo(clss, args);
+		tab.setTag(info);
+		tab.setTabListener(this);
+		tabs.add(info);
+		actionBar.addTab(tab);
+		notifyDataSetChanged();
+	}
+
+	public void setArgs(int position, Bundle args)
+	{
+		tabs.get(position).setArgs(args);
+	}
+
+	// FragmentPagerAdapter
+	// --------------------------------------------------------------------------------------------------------------------------
+
+	@Override
+	public int getCount()
+	{
+		return tabs.size();
+	}
+
+	@Override
+	public Fragment getItem(int position)
+	{
+		TabInfo info = tabs.get(position);
+		return Fragment.instantiate(context, info.clss.getName(), info.args);
+	}
+
+	// ViewPager.OnPageChangeListener
+	// --------------------------------------------------------------------------------------------------------------------------
+
+	@Override
+	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+	{
+	}
+
+	@Override
+	public void onPageSelected(int position)
+	{
+		actionBar.setSelectedNavigationItem(position);
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int state)
+	{
+	}
+
+	// ActionBar.TabListener
+	// --------------------------------------------------------------------------------------------------------------------------
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft)
+	{
+		Object tag = tab.getTag();
+		for (int i = 0; i < tabs.size(); i++)
+		{
+			if (tabs.get(i) == tag)
+			{
+				viewPager.setCurrentItem(i);
+			}
+		}
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft)
+	{
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft)
+	{
+	}
+
+	// TabInfo
+	// --------------------------------------------------------------------------------------------------------------------------
+
+	static final class TabInfo
+	{
+		private final Class<?>	clss;
+		private Bundle			args;
+
+		TabInfo(Class<?> _class, Bundle _args)
+		{
+			clss = _class;
+			args = _args;
+		}
+
+		public void setArgs(Bundle args)
+		{
+			this.args = args;
+		}
+	}
+}
