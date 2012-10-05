0f0aa9f3e016a12666daf15fbf68a848a9c40f06
mvarnagiris <mantas.varnagiris@gmail.com>
2012-10-05 17:15:14 +0100

    Ability to track events from onCreate

--- src/com/anddev/WorkActivity.java
+++ src/com/anddev/WorkActivity.java
@@ -35,6 +35,9 @@ public abstract class WorkActivity extends SherlockFragmentActivity
 		if (eventsToTrackArray != null)
 			for (EventToTrack eventToTrack : eventsToTrackArray)
 				eventsToTrack.put(eventToTrack.event.getEventId(), eventToTrack);
+
+		// Register events
+		registerWorkEvents(true);
 	}
 
 	@Override
@@ -42,33 +45,26 @@ public abstract class WorkActivity extends SherlockFragmentActivity
 	{
 		super.onResume();
 
-		// Register progress work events and update progress.
-		EventToTrack eventToTrack;
-		for (String eventId : eventsToTrack.keySet())
-		{
-			eventToTrack = eventsToTrack.get(eventId);
-			workEventBus.registerForMainThread(this, eventToTrack.event.getClass());
-			if (eventToTrack.showProgress && workEventBus.isWorking(eventId, eventToTrack.workingOnPending))
-				onWorkStarted(eventToTrack.event, eventToTrack.showProgress, true);
-		}
+		// Register events
+		registerWorkEvents(false);
 	}
 
 	@Override
 	protected void onPause()
 	{
-		// Unregister progress work events
-		EventToTrack eventToTrack;
-		for (String eventId : eventsToTrack.keySet())
-		{
-			eventToTrack = eventsToTrack.get(eventId);
-			workEventBus.unregister(this, eventToTrack.event.getClass());
-		}
-
-		// Reset working count and disable progress bar
-		workingCount = 0;
-		setSupportProgressBarIndeterminateVisibility(false);
+		// Unregister events
+		unregisterWorkEvents(false);
 
 		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		// Unregister events
+		unregisterWorkEvents(true);
+
+		super.onDestroy();
 	}
 
 	// Protected methods
@@ -83,19 +79,20 @@ public abstract class WorkActivity extends SherlockFragmentActivity
 	 * This method is not private, because it might be useful to override it to get updates about tasks - new events and restored events.
 	 * </p>
 	 * <p>
-	 * Can be called from {@link WorkActivity#onResume()} when restoring working tasks. If it's called from there, {@code isRestore} will be {@code true},
-	 * because it will not have all values set properly.
+	 * Can be called from {@link WorkActivity#onCreate(Bundle)} or {@link WorkActivity#onResume()} when restoring working tasks. If it's called from there,
+	 * {@code isRealEvent} will be {@code false}, because it will not have all values set properly.
 	 * </p>
 	 * 
 	 * @param event
-	 *            Event. <b>Important: </b>If {@code isRestore} is {@code true}, then this will be the event you return from
+	 *            Event. <b>Important: </b>If {@code isRealEvent} is {@code false}, then this will be the event you return from
 	 *            {@link WorkActivity#getEventsToTrack()}, so it might not have all properties set.
 	 * @param showProgress
 	 *            Flag that indicates if Activity should show progress bar for this event.
-	 * @param isRestore
-	 *            {@code true} means that this method was called from {@link WorkActivity#onResume()} and may not have all properties set.
+	 * @param isRealEvent
+	 *            {@code false} means that this method was called from {@link WorkActivity#onCreate(Bundle)} or {@link WorkActivity#onResume()} and may not have
+	 *            all properties set.
 	 */
-	protected void onWorkStarted(WorkEvent event, boolean showProgress, boolean isRestore)
+	protected void onWorkStarted(WorkEvent event, boolean showProgress, boolean isRealEvent)
 	{
 		if (showProgress)
 		{
@@ -117,8 +114,11 @@ public abstract class WorkActivity extends SherlockFragmentActivity
 	 *            Event.
 	 * @param showProgress
 	 *            Flag that indicates if Activity should show progress bar for this event.
+	 * @param isRealEvent
+	 *            {@code false} means that this method was called from {@link WorkActivity#onDestroy()} or {@link WorkActivity#onPause()} and may not have all
+	 *            properties set. It also means that task is still actually working!
 	 */
-	protected void onWorkFinished(WorkEvent event, boolean showProgress)
+	protected void onWorkFinished(WorkEvent event, boolean showProgress, boolean isRealEvent)
 	{
 		if (showProgress)
 		{
@@ -152,11 +152,11 @@ public abstract class WorkActivity extends SherlockFragmentActivity
 		}
 
 		if (eventToTrack.workingOnPending && event.isPending())
-			onWorkStarted(event, eventToTrack.showProgress, false);
+			onWorkStarted(event, eventToTrack.showProgress, true);
 		else if (!eventToTrack.workingOnPending && event.isStarted())
-			onWorkStarted(event, eventToTrack.showProgress, false);
+			onWorkStarted(event, eventToTrack.showProgress, true);
 		else if (event.isFinished())
-			onWorkFinished(event, eventToTrack.showProgress);
+			onWorkFinished(event, eventToTrack.showProgress, true);
 	}
 
 	/**
@@ -174,20 +174,72 @@ public abstract class WorkActivity extends SherlockFragmentActivity
 		return null;
 	}
 
+	// Private methods
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	private void registerWorkEvents(boolean isOnCreate)
+	{
+		EventToTrack eventToTrack;
+		for (String eventId : eventsToTrack.keySet())
+		{
+			eventToTrack = eventsToTrack.get(eventId);
+			if (isOnCreate && eventToTrack.trackOnCreate || !isOnCreate && !eventToTrack.trackOnCreate)
+			{
+				workEventBus.registerForMainThread(this, eventToTrack.event.getClass());
+				if (workEventBus.isWorking(eventId, eventToTrack.workingOnPending))
+					onWorkStarted(eventToTrack.event, eventToTrack.showProgress, false);
+			}
+		}
+	}
+
+	private void unregisterWorkEvents(boolean isOnDestroy)
+	{
+		EventToTrack eventToTrack;
+		for (String eventId : eventsToTrack.keySet())
+		{
+			eventToTrack = eventsToTrack.get(eventId);
+			if (isOnDestroy && eventToTrack.trackOnCreate || !isOnDestroy && !eventToTrack.trackOnCreate)
+			{
+				workEventBus.unregister(this, eventToTrack.event.getClass());
+				if (workEventBus.isWorking(eventId, eventToTrack.workingOnPending))
+					onWorkFinished(eventToTrack.event, eventToTrack.showProgress, false);
+			}
+		}
+	}
+
 	// EventToTrack
 	// -----------------------------------------------------------------------------------------------------------------------------------
 
+	/**
+	 * Holds information about progress tracking for event.
+	 * 
+	 * @author Mantas Varnagiris
+	 */
 	public static class EventToTrack
 	{
 		public final WorkEvent	event;
 		public final boolean	workingOnPending;
 		public final boolean	showProgress;
+		public final boolean	trackOnCreate;
 
-		public EventToTrack(WorkEvent event, boolean startsOnPending, boolean showProgress)
+		/**
+		 * @param event
+		 *            Event that has correct eventId.
+		 * @param startsOnPending
+		 *            If {@code true}, then event will be considered as working on "pending" status.
+		 * @param showProgress
+		 *            If {@code true} then progress bar will be shown in ActionBar for this event when it's working.
+		 * @param trackOnCreate
+		 *            If {@code true} the event will be registered and restored in {@link WorkActivity#onCreate(Bundle)} and unregistered in
+		 *            {@link WorkActivity#onDestroy()}; otherwise these methods will be used accordingly - {@link WorkActivity#onResume()} and
+		 *            {@link WorkActivity#onPause()}.
+		 */
+		public EventToTrack(WorkEvent event, boolean startsOnPending, boolean showProgress, boolean trackOnCreate)
 		{
 			this.event = event;
 			this.workingOnPending = startsOnPending;
 			this.showProgress = showProgress;
+			this.trackOnCreate = trackOnCreate;
 		}
 	}
 }
