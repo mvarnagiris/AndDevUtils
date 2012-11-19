73431a1d3292c32e9b87ab9a467ac12e8dd66e67
mvarnagiris <mantas.varnagiris@gmail.com>
2012-11-19 10:30:37 +0000

    Updated target API

--- src/com/anddev/utils/APIUtils.java
+++ src/com/anddev/utils/APIUtils.java
@@ -9,4 +9,7 @@ public class APIUtils
 	public static final boolean	SUPPORTS_HONEYCOMB_MR1	= android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB_MR1;
 	public static final boolean	SUPPORTS_HONEYCOMB_MR2	= android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB_MR2;
 	public static final boolean	SUPPORTS_ICS			= android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+	public static final boolean	SUPPORTS_ICS_MR1		= android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1;
+	public static final boolean	SUPPORTS_JELLYBEAN		= android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN;
+	public static final boolean	SUPPORTS_JELLYBEAN_MR1	= android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
 }
